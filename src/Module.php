00e1a58379e9e09910f7670323f559241a1a0501
Mathias Gelhausen <gelhausen@cross-solution.de>
2020-08-27 15:09:23 +0200

    chore: create minimal yawik module structure

---
+++ src/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * YAWIK Sentry Integration
+ *
+ * @see       https://github.com/yawik/sentry for the canonical source repository
+ * @copyright https://github.com/yawik/sentry/blob/master/COPYRIGHT
+ * @license   https://github.com/yawik/sentry/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace YkSentry;
+
+/**
+ * TODO: description
+ *
+ * @author Mathias Gelhausen
+ * TODO: write tests
+ */
+class Module
+{
+
+}
